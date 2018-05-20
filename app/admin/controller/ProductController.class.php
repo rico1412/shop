378008dd89bdb143e5f18ee2c4a6c5d098638b3a
rico-PC\rico <qinhao1412@163.com>
2018-05-20 11:51:37 +0800

    [commit] 后台-商品列表页 完成

--- app/admin/controller/ProductController.class.php
+++ app/admin/controller/ProductController.class.php
@@ -12,14 +12,36 @@ use core\Controller;
 
 class ProductController extends Controller {
     public function showList(){
-        $this->display('product/productIndex.html');
+
+        $model = M('ProductModel');
+
+        $nowPage = isset($_GET['page']) ? $_GET['page'] : 1;
+        $numPerPage = 5;
+
+        $sql = "select count(*) AS num from sp_goods_info where 1";
+
+        $num = $model->getRow($sql)['num'];
+
+        $totalPage = (int)ceil($num / $numPerPage);
+
+        $url = C('URL.main') . "/index.php?p=admin&m=product&a=showList&page";
+        $pageHtml = pageHtml($nowPage, $totalPage, $url);
+
+        $x = ($nowPage - 1) * $numPerPage;
+        $sql = "select p.id, p.name, p.ori_price, p.price, p.status, p.add_time, c.name AS c_name from sp_goods_info AS p join sp_category AS c on p.category_id=c.id order by add_time desc limit {$x}, {$numPerPage}";
+
+        $p_data = $model->getRows($sql);
+
+        $this->assign('pageHtml', $pageHtml);
+        $this->assign('p_data', $p_data);
+        $this->display('product/list.html');
     }
 
     public function showAdd(){
-        $this->display('product/productAdd.html');
+        //$this->display('product/productAdd.html');
     }
 
     public function showEdit(){
-        $this->display('product/productEdit.html');
+        //$this->display('product/productEdit.html');
     }
 }
