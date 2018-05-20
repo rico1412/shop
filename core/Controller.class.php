cd776e0048f673094f655de33580056cab9310c5
rico-PC\rico <qinhao1412@163.com>
2018-05-20 21:59:11 +0800

    [fix] 登录逻辑优化

--- core/Controller.class.php
+++ core/Controller.class.php
@@ -6,24 +6,6 @@ use \Smarty;//引入 全局空间  下的 Smarty类
 class Controller extends Smarty{
     //protected $smarty;
     public function __construct(){
-        @session_start();
-        $do =$GLOBALS['plat']=='home'||$GLOBALS['module']=='Privilege';
-        if(!isset($_SESSION['admin'])&&!$do){
-            if(isset($_COOKIE['rememberMe'])){//表示登陆过7天免登录
-                $id =$_COOKIE['rememberMe'];
-                $UserModel =M('UserModel');
-                $sql ="select * from sp_user where id='{$id}'";
-                $admin=$UserModel->getRow($sql);
-
-                $_SESSION['admin']=$admin;
-            }else{
-                
-                echo "你需要先登录";
-                $url = C('URL.main')."/index.php/?p=admin&m=Privilege&a=showLogin";
-                header('Refresh:2;url='.$url);
-                exit();
-            }
-        }
 
         #解决父类构造方法被重写
         parent::__construct();
@@ -48,9 +30,27 @@ class Controller extends Smarty{
 
         $this->left_delimiter = '{<';
         $this->right_delimiter = '>}';
+
+        @session_start();
+        $do = $GLOBALS['plat'] == 'home' || $GLOBALS['module'] == 'Privilege';
+        if (!isset($_SESSION['admin']) && !$do) {
+            if (isset($_COOKIE['rememberMe'])) {//表示登陆过7天免登录
+                $id = $_COOKIE['rememberMe'];
+                $UserModel = M('UserModel');
+                $sql = "select * from sp_user where id='{$id}'";
+                $admin = $UserModel->getRow($sql);
+
+                $_SESSION['admin'] = $admin;
+            } else {
+                $str = "你需要先登录";
+                $url = C('URL.main') . "/index.php/?p=admin&m=Privilege&a=showLogin";
+                $this->showTips(false, $str, $url);
+                exit();
+            }
+        }
     }
 
-    public function showTips($ok=true, $tips='操作成功', $jumpUrl='', $waitSecond=2){
+    public function showTips($ok = true, $tips = '操作成功', $jumpUrl = '', $waitSecond = 2){
 
         $this->assign('tips', $tips);
         $this->assign('ok', $ok);
