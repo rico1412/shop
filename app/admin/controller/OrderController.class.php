52c03ae3219068dfdc57c54634fe334bb187134c
rico-PC\rico <qinhao1412@163.com>
2018-05-22 09:59:34 +0800

    [commit] 修改订单状态功能完成

--- app/admin/controller/OrderController.class.php
+++ app/admin/controller/OrderController.class.php
@@ -17,8 +17,7 @@ class OrderController extends Controller {
 
         # 分页相关
         $nowPage = isset($_GET['page']) ? $_GET['page'] : 1;
-        var_dump($nowPage);
-        $numPerPage = 1;
+        $numPerPage = 5;
         $sql = "select count(*) AS num from sp_order where 1";
         $num = $model->getRow($sql)['num'];
         $totalPage = (int)ceil($num / $numPerPage);
@@ -49,5 +48,25 @@ class OrderController extends Controller {
         $this->assign('page', $page);
         $this->assign('details', $details);
         $this->display('order/details_list.html');
+    }
+
+    public function upd_h(){
+
+        $id = $_GET['id'];
+        $nowPage = $_GET['page'];
+
+        $sql = "update sp_order set status=1 where id={$id}";
+
+        $model = M('OrderModel');
+        $re = $model->setData($sql);
+
+        if ($re) {
+            $str = "订单已处理";
+        } else {
+            $str = "订单处理失败，请联系管理员！";
+        }
+
+        $url = C('URL.main') . "/index.php?p=admin&m=order&a=showList&page={$nowPage}";
+        $this->showTips($re, $str, $url);
     }
 }
